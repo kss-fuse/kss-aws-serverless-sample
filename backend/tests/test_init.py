01f2865e5276481e41d85a6e6e86758c063ddbea
kss-fuse <h_fuse@systemsolution.co.jp>
2026-04-06 20:04:22 +0900

    jwt関連を無視

--- backend/tests/test_init.py
+++ backend/tests/test_init.py
@@ -2,9 +2,9 @@ from unittest.mock import patch
 from src.functions.init.app import lambda_handler
 
 
-@patch("src.functions.init.app.get_jwks")
-@patch("src.functions.init.app.jwt.decode")
-@patch("src.functions.init.app.jwt.get_unverified_header")
+# @patch("src.functions.init.app.get_jwks")
+# @patch("src.functions.init.app.jwt.decode")
+# @patch("src.functions.init.app.jwt.get_unverified_header")
 def test_lambda_handler(mock_header, mock_decode, mock_get_jwks):
 
     mock_get_jwks.return_value = {
